0677352ef2cae2f91a9cc96d4c78b1cc88ed0142
breezjirasak <jirasak.tu@ku.th>
2025-05-11 00:15:10 +0700

    init bookmark id

--- internal/model/bookmark.go
+++ internal/model/bookmark.go
@@ -1,5 +1,10 @@
 package model
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 // Bookmark represents a trip bookmarked by a user (one-to-many).
 type Bookmark struct {
 	ID string `gorm:"primaryKey;type:char(36)" json:"bookmark_id"`
@@ -8,4 +13,13 @@ type Bookmark struct {
 
 	User User `gorm:"foreignKey:UserID;references:ID;constraint:OnDelete:CASCADE;" json:"user"`
 	Trip Trip `gorm:"foreignKey:TripID;references:ID;constraint:OnDelete:CASCADE;" json:"trip"`
+}
+
+
+func (b *Bookmark) BeforeCreate(tx *gorm.DB) error {
+	// Generate UUID if not provided
+	if b.ID == "" {
+		b.ID = uuid.New().String()
+	}
+	return nil
 }
